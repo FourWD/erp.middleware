09f60e0c7f1a3f3a053bbf5b45973b34ebca88af
p-amu <99467582+p-amu@users.noreply.github.com>
2026-02-03 15:46:43 +0700

    Update vehicle-colors.go
    add image path for booking

--- orm/vehicle-colors.go
+++ orm/vehicle-colors.go
@@ -7,5 +7,6 @@ import (
 type VehicleColors struct {
 	midOrm.VehicleColor
 	ColorCode string `json:"color_code" query:"color_code" gorm:"type:varchar(10)"`
-	Code string `json:"code" query:"code" gorm:"type:varchar(4)"`
+	Code      string `json:"code" query:"code" gorm:"type:varchar(4)"`
+	ImagePath string `json:"image_path" query:"image_path" gorm:"type:varchar(1024)"`
 }
