10d4a5f5c3176da68ab5e663b53f2d6a33c64cbc
p-amu <99467582+p-amu@users.noreply.github.com>
2026-02-18 11:47:38 +0700

    Update booking-cancel-reason.go
    booking cancel reason

--- orm/booking-cancel-reason.go
+++ orm/booking-cancel-reason.go
@@ -5,8 +5,10 @@ import "github.com/FourWD/middleware/model"
 // midOrm "github.com/FourWD/middleware/orm"
 
 type BookingCancelReason struct { //
-	ID string `json:"id" query:"id" gorm:"type:varchar(2); primary_key"`
+	ID string `json:"id" query:"id" gorm:"type:varchar(3); primary_key"`
 	model.GormModel
+
+	BookingStatusID string `json:"booking_status_id" query:"booking_status_id" gorm:"type:varchar(2)"`
 
 	Name string `json:"name" query:"name" gorm:"type:varchar(256)"`
 	model.GormRowOrder
