1219fe1cba8e3918b6949adeefcb63243da43b72
p-amu <99467582+p-amu@users.noreply.github.com>
2026-02-19 12:15:36 +0700

    Update booking.go
    update booking upload to 10

--- orm/booking.go
+++ orm/booking.go
@@ -28,30 +28,30 @@ type Booking struct {
 	CustomerUseTypeID string `json:"customer_use_type_id" query:"customer_use_type_id" gorm:"type:varchar(2)"` //เหตุผลการซื้อทำ master
 
 	// show user_type_id
-	ReferralChannelID string     `json:"referral_channel_id" query:"referral_channel_id" gorm:"type:varchar(2)"`
-	ReferralName      string     `json:"referral_name" query:"referral_name" gorm:"type:varchar(256)"`
-	ReserveDate       *time.Time `json:"reserve_date" query:"reserve_date"`                                 // วันที่รับเงินจอง
-	ReserveBy         string     `json:"reserve_by" query:"reserve_by" gorm:"type:varchar(36)"`             // คนรับเงินจอง
-	ReserveDeposit    float64    `json:"reserve_deposit" query:"reserve_deposit" gorm:"type:decimal(14,2)"` // จำนวนเงินจอง
+	ReferralChannelID string     `json:"referral_channel_id" query:"referral_channel_id" gorm:"type:varchar(2)"` // ช่องทางการแนะนํา
+	ReferralName      string     `json:"referral_name" query:"referral_name" gorm:"type:varchar(256)"`           // ชื่อผู้แนะนํา
+	ReserveDate       *time.Time `json:"reserve_date" query:"reserve_date"`                                      // วันที่รับเงินจอง
+	ReserveBy         string     `json:"reserve_by" query:"reserve_by" gorm:"type:varchar(36)"`                  // คนรับเงินจอง
+	ReserveDeposit    float64    `json:"reserve_deposit" query:"reserve_deposit" gorm:"type:decimal(14,2)"`      // จำนวนเงินจอง
 	// CaseSubmissionDate *time.Time `json:"case_submission_date" query:"case_submission_date"`                 // วันที่ส่งเคส
-	ExpectDeliveryDate *time.Time `json:"expect_delivery_date" query:"expect_delivery_date"` // วันที่ต้องการจัดส่ง
-	DeliveryDate       *time.Time `json:"delivery_date" query:"delivery_date" gorm:"type:date"`
-	DeliveryLocation   string     `json:"delivery_location" query:"delivery_location" gorm:"type:varchar(500)"`
-	RemarkDelivery     string     `json:"remark_delivery" query:"remark_delivery" gorm:"type:text"`
+	ExpectDeliveryDate *time.Time `json:"expect_delivery_date" query:"expect_delivery_date"`                    // วันที่คาดว่าจัดส่ง
+	DeliveryDate       *time.Time `json:"delivery_date" query:"delivery_date" gorm:"type:date"`                 // วันที่จัดส่ง
+	DeliveryLocation   string     `json:"delivery_location" query:"delivery_location" gorm:"type:varchar(500)"` // สถานที่จัดส่ง
+	RemarkDelivery     string     `json:"remark_delivery" query:"remark_delivery" gorm:"type:text"`             // หมายเหตุการจัดส่ง
 
 	IsDownPaid  bool    `json:"is_down_paid" query:"is_down_paid" gorm:"type:bool"` // จ่ายเงินจองเรียบร้อย
-	IsCancel    bool    `json:"is_cancel" query:"is_cancel" gorm:"type:bool"`
+	IsCancel    bool    `json:"is_cancel" query:"is_cancel" gorm:"type:bool"`       // ยกเลิก
 	Remark      string  `json:"remark" query:"remark" gorm:"type:text"`
 	PricePreVat float64 `json:"price_pre_vat" query:"price_pre_vat" gorm:"type:decimal(14,2)"`
 	Vat         float64 `json:"vat" query:"vat" gorm:"type:decimal(14,2)"`
 	Price       float64 `json:"price" query:"price" gorm:"type:decimal(14,2)"`
 	// k'pom special discount
-	Discount              float64    `json:"discount" query:"discount" gorm:"type:decimal(14,2)"`
-	SpecialDiscount       float64    `json:"special_discount" query:"is_special_discount" gorm:"type:decimal(14,2)"`
-	SpecialDiscountBy     string     `json:"special_discount_by" query:"special_discount_by" gorm:"type:varchar(36)"`
-	SpecialDiscountDate   *time.Time `json:"special_discount_date" query:"special_discount_date" gorm:"type:date"`
-	SpecialDescountRef    string     `json:"special_descount_ref" query:"special_descount_ref" gorm:"type:text"`
-	SpecialDiscountRemark string     `json:"special_discount_remark" query:"special_discount_remark" gorm:"type:text"`
+	Discount              float64    `json:"discount" query:"discount" gorm:"type:decimal(14,2)"`                      // ส่วนลด
+	SpecialDiscount       float64    `json:"special_discount" query:"is_special_discount" gorm:"type:decimal(14,2)"`   // ส่วนลดพิเศษ
+	SpecialDiscountBy     string     `json:"special_discount_by" query:"special_discount_by" gorm:"type:varchar(36)"`  // ส่วนลดพิเศษโดย
+	SpecialDiscountDate   *time.Time `json:"special_discount_date" query:"special_discount_date" gorm:"type:date"`     // วันที่ส่วนลด
+	SpecialDiscountRef    string     `json:"special_discount_ref" query:"special_discount_ref" gorm:"type:text"`       // หมายเหตุ
+	SpecialDiscountRemark string     `json:"special_discount_remark" query:"special_discount_remark" gorm:"type:text"` // หมายเหตุ
 
 	// discount booking
 	// DiscountVehicle        float64 `json:"discount_vehicle" query:"discount_vehicle" gorm:"type:decimal(14,2)"`
@@ -75,9 +75,14 @@ type Booking struct {
 	VehicleSubModelID        string `json:"vehicle_sub_model_id" query:"vehicle_sub_model_id" gorm:"type:varchar(36)"`
 	VehicleColorID           string `json:"vehicle_color_id" query:"vehicle_color_id" gorm:"type:varchar(36)"`
 	VehicleYearManufacturing string `json:"vehicle_year_manufacturing" query:"vehicle_year_manufacturing" gorm:"type:varchar(4)"`
-	UploadPath1              string `json:"upload_path_1" query:"upload_path_1" gorm:"type:varchar(500)"`
-	UploadPath2              string `json:"upload_path_2" query:"upload_path_2" gorm:"type:varchar(500)"`
-	UploadPath3              string `json:"upload_path_3" query:"upload_path_3" gorm:"type:varchar(500)"`
-	UploadPath4              string `json:"upload_path_4" query:"upload_path_4" gorm:"type:varchar(500)"`
-	UploadPath5              string `json:"upload_path_5" query:"upload_path_5" gorm:"type:varchar(500)"`
+	UploadPath1              string `json:"upload_path_1" query:"upload_path_1" gorm:"column:upload_path_1;type:varchar(500)"`
+	UploadPath2              string `json:"upload_path_2" query:"upload_path_2" gorm:"column:upload_path_2;type:varchar(500)"`
+	UploadPath3              string `json:"upload_path_3" query:"upload_path_3" gorm:"column:upload_path_3;type:varchar(500)"`
+	UploadPath4              string `json:"upload_path_4" query:"upload_path_4" gorm:"column:upload_path_4;type:varchar(500)"`
+	UploadPath5              string `json:"upload_path_5" query:"upload_path_5" gorm:"column:upload_path_5;type:varchar(500)"`
+	UploadPath6              string `json:"upload_path_6" query:"upload_path_6" gorm:"column:upload_path_6;type:varchar(500)"`
+	UploadPath7              string `json:"upload_path_7" query:"upload_path_7" gorm:"column:upload_path_7;type:varchar(500)"`
+	UploadPath8              string `json:"upload_path_8" query:"upload_path_8" gorm:"column:upload_path_8;type:varchar(500)"`
+	UploadPath9              string `json:"upload_path_9" query:"upload_path_9" gorm:"column:upload_path_9;type:varchar(500)"`
+	UploadPath10             string `json:"upload_path_10" query:"upload_path_10" gorm:"column:upload_path_10;type:varchar(500)"`
 }
