5f6c9944bf3318e60498027a7a7f387186e53034
Narongrit Mekpho <narongritjamenrtmp@gmail.com>
2026-02-20 13:47:35 +0700

    Update referal-channel-sub.go

--- orm/referal-channel-sub.go
+++ orm/referal-channel-sub.go
@@ -10,5 +10,6 @@ type ReferalChannelSub struct { //
 
 	Name             string `json:"name" query:"name" gorm:"type:varchar(256)"` // line face
 	ReferalChannelID string `json:"referal_channel_id" query:"referal_channel_id" gorm:"type:varchar(2)"`
+	SubCode          string `json:"sub_code" query:"sub_code" gorm:"type:varchar(2)"`
 	model.GormRowOrder
 }
