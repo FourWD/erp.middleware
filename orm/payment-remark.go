960c8f52a05a297dc51f9ef0742e80e0469ee0cb
Narongrit Mekpho <narongritjamenrtmp@gmail.com>
2026-02-24 16:13:47 +0700

    update orm

---
+++ orm/payment-remark.go
@@ -0,0 +1,14 @@
+package orm
+
+import "github.com/FourWD/middleware/model"
+
+// midOrm "github.com/FourWD/middleware/orm"
+
+type PaymentRemark struct { //เหตุผล การชําระเงินจอง
+	ID string `json:"id" query:"id" gorm:"type:varchar(2); primary_key"`
+	model.GormModel
+
+	Name   string `json:"name" query:"name" gorm:"type:varchar(256)"`
+	NameEn string `json:"name_en" query:"name_en" gorm:"type:varchar(256)"`
+	model.GormRowOrder
+}
