41007914de0df77c20d8c3cb4b24965d64c8ff91
Narongrit Mekpho <narongritjamenrtmp@gmail.com>
2026-02-24 16:28:34 +0700

    update

--- orm/no-reserve-remark.go
+++ orm/no-reserve-remark.go
@@ -4,7 +4,7 @@ import "github.com/FourWD/middleware/model"
 
 // midOrm "github.com/FourWD/middleware/orm"
 
-type PaymentRemark struct { //เหตุผล การชําระเงินจอง
+type NoReserveRemark struct { //เหตุผล การชําระเงินจอง
 	ID string `json:"id" query:"id" gorm:"type:varchar(2); primary_key"`
 	model.GormModel
 
