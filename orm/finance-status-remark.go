2d0a291a30b69c943687d641536305627ee885a1
Narongrit Mekpho <narongritjamenrtmp@gmail.com>
2026-03-06 14:03:26 +0700

    Update finance-status-remark.go

--- orm/finance-status-remark.go
+++ orm/finance-status-remark.go
@@ -6,9 +6,8 @@ type FinanceStatusRemark struct {
 	ID string `json:"id" query:"id" gorm:"type:varchar(2);primary_key"`
 	model.GormModel
 
-	Name            string `json:"name" query:"name" gorm:"type:varchar(50)"`
-	NameEn          string `json:"name_en" query:"name_en" gorm:"type:varchar(50)"`
-	FinanceStatusID string `json:"finance_status_id" query:"finance_status_id" gorm:"type:varchar(2);"`
+	Name   string `json:"name" query:"name" gorm:"type:varchar(50)"`
+	NameEn string `json:"name_en" query:"name_en" gorm:"type:varchar(50)"`
 
 	model.GormRowOrder
 }
