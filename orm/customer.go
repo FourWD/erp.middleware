43a7ac801c92aa24e2c55a8927dc5b84bca4fcec
Chayakorn SaeEwia <c.chayakorn86@gmail.com>
2026-03-12 13:34:44 +0700

    modify column name

--- orm/customer.go
+++ orm/customer.go
@@ -85,10 +85,10 @@ type Customer struct {
 	ContactPersonFacebook     string `json:"contact_person_facebook" query:"contact_person_facebook" gorm:"type:varchar(500)"`
 
 	IdCardPath  string `json:"id_card_path" query:"id_card_path" gorm:"type:text"`
-	IdCardPath1 string `json:"id_card_path_1" query:"id_card_path_1" gorm:"type:text"`
-	IdCardPath2 string `json:"id_card_path_2" query:"id_card_path_2" gorm:"type:text"`
-	IdCardPath3 string `json:"id_card_path_3" query:"id_card_path_3" gorm:"type:text"`
-	IdCardPath4 string `json:"id_card_path_4" query:"id_card_path_4" gorm:"type:text"`
+	IdCardPath1 string `json:"id_card_path_1" query:"id_card_path_1" gorm:"column:id_card_path_1;type:text"`
+	IdCardPath2 string `json:"id_card_path_2" query:"id_card_path_2" gorm:"column:id_card_path_2;type:text"`
+	IdCardPath3 string `json:"id_card_path_3" query:"id_card_path_3" gorm:"column:id_card_path_3;type:text"`
+	IdCardPath4 string `json:"id_card_path_4" query:"id_card_path_4" gorm:"column:id_card_path_4;type:text"`
 
 	IsAcceptConsent bool   `json:"is_accept_consent" query:"is_accept_consent" gorm:"type:bool"`
 	ConsentID       string `json:"consent_id" query:"consent_id" gorm:"type:varchar(36)"`
