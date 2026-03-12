a88687933e4efafe398fa010631d963b368052c4
Chayakorn SaeEwia <c.chayakorn86@gmail.com>
2026-03-12 12:04:45 +0700

    upadte booking status

--- orm/booking-status-remark.go
+++ orm/booking-status-remark.go
@@ -8,6 +8,8 @@ type BookingStatusRemark struct { //no money
 	ID string `json:"id" query:"id" gorm:"type:varchar(2); primary_key"`
 	model.GormModel
 
+	BookingStatusID string `json:"booking_status_id" query:"booking_status_id" gorm:"type:varchar(2)"`
+
 	Name string `json:"name" query:"name" gorm:"type:varchar(256)"`
 	model.GormRowOrder
 }
